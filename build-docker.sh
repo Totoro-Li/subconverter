2a1be6e117daf645e57f9be6dcbb5a1957e01f4e
Root <test@test.com>
2023-10-12 11:16:34 +0000

    update

---
+++ build-docker.sh
@@ -0,0 +1,8 @@
+# build with this Dockerfile and tag it subconverter-custom
+docker build -t subconverter-custom:latest .
+# run the docker detached, forward internal port 25500 to host port 25500
+docker run -d --restart=always -p 25500:25500 subconverter-custom:latest
+# then check its status
+curl http://localhost:25500/version
+# if you see `subconverter vx.x.x backend` then the container is up and running
+curl -F "data=@pref.ini" http://localhost:25500/updateconf?type=form\&token=password
