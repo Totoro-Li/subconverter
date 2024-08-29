16ef880f472049986ed25b9a87c275fa3bfc7645
root <90897313+Totoro-Li@users.noreply.github.com>
2024-08-29 10:38:13 +0800

    Backup

--- full-build.sh
+++ full-build.sh
@@ -1,9 +1,8 @@
-docker stop subconverter
-docker rm subconverter
+docker compose down
 # build with this Dockerfile and tag it subconverter-custom
 docker build -t subconverter-custom:latest -f scripts/Dockerfile .
 # run the docker detached, forward internal port 25500 to host port 25500
-docker run -d --restart=always --name subconverter --network main -p 25500:25500 subconverter-custom:latest
+docker compose up -d
 # then check its status
 
 sleep 1
